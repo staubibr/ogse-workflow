7ad40960297e61dc73f0290454a4fe9cb9bd26ae
staubibr <staubin.bruno@gmail.com>
2023-03-29 23:30:45 -0400

    Fixed issue with task select (did not use the same pattern as other tasks with where clause)

--- tasks/task_select.py
+++ tasks/task_select.py
@@ -3,15 +3,22 @@ from components.task import Task, TaskParameters
 
 class TaskSelectParams(TaskParameters):
 
+    # @property
+    # def where(self):
+    #     """Object with criteria {field1: value1, field2: value2, etc} is returned as a single where clause."""
+    #     w = []
+    #
+    #     for key, value in self.json["where"].items():
+    #         w.append(f"{key}='{value}'")  # postgres is matching numbers with or without quotes
+    #
+    #     return " AND ".join(w)
+    
     @property
     def where(self):
-        """Object with criteria {field1: value1, field2: value2, etc} is returned as a single where clause."""
-        w = []
+        if "where" not in self.json:
+            return ""
 
-        for key, value in self.json["where"].items():
-            w.append(f"{key}='{value}'")  # postgres is matching numbers with or without quotes
-
-        return " AND ".join(w)
+        return "WHERE {0} IN ({1})".format(self.json["where"]["field"], ",".join(self.json["where"]["value"]))
 
     def __init__(self, json):
         super().__init__(json)
@@ -40,4 +47,4 @@ class TaskSelect(Task):
         if self.source is None:
             raise Exception("Select task requires a source parameter.")
 
-        db.execute("CREATE TABLE {0} AS (SELECT * FROM {1} WHERE {2})".format(self.output, self.source, params.where))
+        db.execute("CREATE TABLE {0} AS (SELECT * FROM {1} {2})".format(self.output, self.source, params.where))
